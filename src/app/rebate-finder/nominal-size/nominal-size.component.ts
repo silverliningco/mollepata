d7922e9a45cfeac73f56c27bfacc54a82a3be4f3
Ward Welvaert <ward.welvaert@gmail.com>
2022-09-26 08:39:50 -0400

    Create heated-cooled and nominal-size components.

---
+++ src/app/rebate-finder/nominal-size/nominal-size.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nominal-size',
+  templateUrl: './nominal-size.component.html',
+  styleUrls: ['./nominal-size.component.css']
+})
+
+export class NominalSizeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
