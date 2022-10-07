f5ed4b5a3b73ffebae7493fa7034d1069454a94c
esperanzaWG <myfrick57@gmail.com>
2022-10-07 14:06:56 -0500

    ngFor card componet

--- src/app/equipment-search/hvac-results/card/card.component.ts
+++ src/app/equipment-search/hvac-results/card/card.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 
 export interface OutdoorUnits {
-  cod: string
+  id: string,
+  indoor: string[]
 }
+
 
 @Component({
   selector: 'app-card',
@@ -11,28 +13,13 @@ export interface OutdoorUnits {
 })
 export class CardComponent implements OnInit {
 
-  @Input() major = 0;
-  @Input() minor = 0;
-  changeLog: string[] = [];
+  @Input() indoor!: string[];
+  @Input('master') masterName = '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    const log: string[] = [];
-    for (const propName in changes) {
-      const changedProp = changes[propName];
-      const to = JSON.stringify(changedProp.currentValue);
-      if (changedProp.isFirstChange()) {
-        log.push(`Initial value of ${propName} set to ${to}`);
-      } else {
-        const from = JSON.stringify(changedProp.previousValue);
-        log.push(`${propName} changed from ${from} to ${to}`);
-      }
-    }
-    this.changeLog.push(log.join(', '));
-  }
-
+ 
 }
--- src/app/equipment-search/rebate-finder/rebate-finder.component.ts
+++ src/app/equipment-search/rebate-finder/rebate-finder.component.ts
@@ -2,6 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { bridgeService } from '../services/bridge.service';
 
 import { Location, ListUtilities, DwellingInfo, ConstructionType } from '../models/rebate-finder-inputs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+// prueva
+export interface OutdoorUnits {
+  id: string,
+  indoor: string[]
+}
+
+export const OUTDOORS = [
+  {
+    id: '25VNA424A003',
+    indoor: ['FE4ANB003L00', 'FE4ANB005L00', 'FE4ANF002L00']
+  },
+  {
+    id: '25HPB630A003',
+    indoor: ['FX4DNB037L00 ', 'B4CNP030L00']
+  },
+  {
+    id: '24VNA624A003',
+    indoor: ['CAPMP3017ALA', 'CNPHP4321ALA', 'CNPVP2417ALA', 'CAPMP3617ALA']
+  },
+];
+// prueva
 
 @Component({
   selector: 'app-rebate-finder',
@@ -17,48 +40,82 @@ export class RebateFinderComponent implements OnInit {
   desableButton: boolean = true;
 
   // prueva 
-  major = 1;
-  minor = 23;
-
-  newMinor() {
-    this.minor++;
-  }
-
-  newMajor() {
-    this.major++;
-    this.minor = 0;
-  }
+  outdoorGroup !: FormGroup;
+  outdoorUnits: OutdoorUnits[] = OUTDOORS;
+  indoors: any[] = [];
+  master = 'Master';
+  // prueva
 
   constructor(
-    public _bridge: bridgeService
+    public _bridge: bridgeService,
+    private formBuilder: FormBuilder // prueva
   ) { }
 
   ngOnInit(): void {
     this._bridge.sentLocationParams
         .subscribe((payload: any) => {
-        
           this.myLocation = payload.data;
-          // component button
-          // this._bridge.buttonContinue.emit({
-          //   data: this.myLocation
-          //  })
           this.ParamsRebateEligibility();
           this.ActiveContinuebutton(this.myLocation);  
          });
     
-  this._bridge.dwellingInfoParams
+    this._bridge.dwellingInfoParams
          .subscribe((payload: any) => {
-         
            this.dwellingInfo = payload.data;
-           // component button
-          //  this._bridge.buttonContinue.emit({
-          //   data: this.dwellingInfo
-          //  })
-
           this.ParamsRebateEligibility();
           this.ActiveContinuebutton(this.dwellingInfo);                    
           });
+    
+    // prueva
+    this.outdoorGroup = this.formBuilder.group({
+      outdoor: [ null, Validators.required]
+    }); 
+    // prueva  
+    
   }
+
+  // prueva
+  changeOutdoor(){
+    let selectOutdoor =  this.outdoorGroup.controls['outdoor'].value;
+
+    let combination = this.SearchInResponses(OUTDOORS, ['id'], selectOutdoor);
+    // console.log(combination[0].indoor);
+    this.indoors = combination[0].indoor;
+    
+
+  }
+
+  SearchInResponses (objectData:Array<any>,  combinations: Array<any>, unit: any) {
+    
+    let input = unit;
+    let result: Array<any> = [];
+  
+    
+      let b = objectData.filter((data:any) => {
+        let combinationQueries = "";
+    
+        combinations.forEach((arg:any) => {
+          combinationQueries +=
+          data.hasOwnProperty(arg) && data[arg].trim() + "";
+        });
+    
+        return Object.keys(data).some((key:any) => {
+          return(
+            (data[key] != undefined && 
+              data[key] != null && 
+              JSON.stringify(data[key]).trim().includes(input)) ||
+            combinationQueries.trim().includes(input)  
+          );
+        });
+      });
+    
+      if(b.length != 0){
+        result = b
+      }
+    
+    return result;
+  }
+  // prueva
 
 
   ActiveContinuebutton(input:any): boolean{
