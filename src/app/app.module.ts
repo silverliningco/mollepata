73d0202979f6df69a48dbef9539c6363b4bb02de
esperanzaWG <myfrick57@gmail.com>
2022-10-07 14:16:22 -0500

    all steps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { BasicEstructureModule } from './basic-estructure/basic-estructure.module';
-import { PadreComponent } from './prueba/padre/padre.component';
-import { HijoComponent } from './prueba/padre/hijo/hijo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PadreComponent,
-    HijoComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
