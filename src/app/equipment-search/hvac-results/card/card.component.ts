19d2194b00e7e503803223567cc997c340a5794a
esperanzaWG <myfrick57@gmail.com>
2022-10-11 14:12:26 -0500

    boss idea

--- src/app/equipment-search/hvac-results/card/card.component.ts
+++ src/app/equipment-search/hvac-results/card/card.component.ts
@@ -13,13 +13,23 @@ export interface OutdoorUnits {
 })
 export class CardComponent implements OnInit {
 
-  @Input() indoor!: string[];
+  @Input() outdoorUnits!: string[];
   @Input('master') masterName = '';
+// output: max rebate value
+
+  selectedOutdoorUnit: string;
+
+  results: [];
 
   constructor() { }
 
   ngOnInit(): void {
+    // If length of outdoorUnits == 1, call this.selectedOutdoorUnit(outdoorUnits[0])
+    // If length of outdoorUnits > 1, nothing else is required here.
   }
 
- 
+ function selectOutdoorUnit(someOutdoorUnit) {
+    this.selectedOutdoorUnit = someOutdoorUnit;
+    // Load all the results for this outdoor unit, then render the data in the card.
+ }
 }
--- src/app/equipment-search/rebate-finder/rebate-finder.component.ts
+++ src/app/equipment-search/rebate-finder/rebate-finder.component.ts
@@ -10,26 +10,13 @@ export interface OutdoorUnits {
   indoor: string[]
 }
 
-export const OUTDOORS = [
-  {
-    id: '25VNA424A003',
-    indoor: ['FE4ANB003L00', 'FE4ANB005L00', 'FE4ANF002L00']
-  },
-  {
-    id: '25HPB630A003',
-    indoor: ['FX4DNB037L00 ', 'B4CNP030L00']
-  },
-  {
-    id: '24VNA624A003',
-    indoor: ['CAPMP3017ALA', 'CNPHP4321ALA', 'CNPVP2417ALA', 'CAPMP3617ALA']
-  },
-];
+export const OUTDOORS = [ '25VNA424A003', '25HPB630A003', '24VNA624A003'];
 // prueva
 
 // talves no considerar desableButton?
-export const INPUTS = {
-  'Location': ['state', 'utilityProviders', 'desableButton'],
-   'DwellingInfo': ['fuelSource', 'ConstructionType', 'desableButton']
+export const INPUTS_PROPERTIES = {
+  'myLocation': ['state', 'utilityProviders', 'desableButton'],
+  'myDwellingInfo': ['fuelSource', 'ConstructionType', 'desableButton']
 }
 
 @Component({
@@ -42,13 +29,13 @@ export class RebateFinderComponent implements OnInit {
 
   // local variables save data of stepper
   myLocation: Location = new Location(null, new ListUtilities(null, null), true); 
-  dwellingInfo: DwellingInfo = new DwellingInfo(null, null, true)
+  myDwellingInfo: DwellingInfo = new DwellingInfo(null, null, true)
 
   desableButton: boolean = true;
 
   // prueva 
   outdoorGroup !: FormGroup;
-  outdoorUnits: OutdoorUnits[] = OUTDOORS;
+  outdoorUnits: string[] = OUTDOORS;
   indoors: any[] = [];
   master = 'Master';
   // prueva
@@ -69,15 +56,15 @@ export class RebateFinderComponent implements OnInit {
     
     this._bridge.dwellingInfoParams
          .subscribe((payload: any) => {
-           this.dwellingInfo = payload.data[0];
-           this.dwellingInfo.desableButton = payload.data[1];
+           this.myDwellingInfo = payload.data[0];
+           this.myDwellingInfo.desableButton = payload.data[1];
           this.ParamsRebateEligibility();                    
           });
     
     // prueva
-    this.outdoorGroup = this.formBuilder.group({
+    /* this.outdoorGroup = this.formBuilder.group({
       outdoor: [ null, Validators.required]
-    }); 
+    });  */
     // prueva  
     
   }
@@ -131,7 +118,7 @@ export class RebateFinderComponent implements OnInit {
         fossilFuelUtilityId: this.myLocation.utilityProviders?.fossilFuelUtilityId
       },
       state: this.myLocation.state,
-      fuelSource: this.dwellingInfo.fuelSource
+      fuelSource: this.myDwellingInfo.fuelSource
     }
 
     this._bridge.paramsRebateEligibility.emit({
@@ -141,13 +128,20 @@ export class RebateFinderComponent implements OnInit {
   }
 
   SearchType(payload: object): void {
-    let allInputs = Object.values(INPUTS);
+    let allInputs = Object.values(INPUTS_PROPERTIES);
     let myInputs = Object.keys(payload)
 
     let equal!: boolean;
 
+    /* for (const key in INPUTS_PROPERTIES) {
+      console.log(key);
+      if (key == 'myLocation'){
+        this.key = 
+      }
+    }
+ */
     // comparar si los 2 arreglos son iguales 
-    for (const iAll of allInputs) {
+    /* for (const iAll of allInputs) {
       for (const ieachInput of iAll) {
         for (const iMyInputs of myInputs) {
           if (ieachInput === iMyInputs){
@@ -156,7 +150,7 @@ export class RebateFinderComponent implements OnInit {
         }
         
       }
-    }
+    } */
 
   }
 
