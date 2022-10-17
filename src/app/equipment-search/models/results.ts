fc38fc40cf3f6bbbc1b080a875e6ddea0ae3a023
esperanzaWG <myfrick57@gmail.com>
2022-10-17 13:28:18 -0500

    clean models

--- src/app/equipment-search/models/results.ts
+++ src/app/equipment-search/models/results.ts
@@ -31,27 +31,3 @@ export class Components {
     ) {}
 }
 
-export class Card {
-    constructor(
-        public outdoorUnit: string | null,
-        public optionsUnit: any[] | null,
-        public EER: number | null,
-        public AFUE: number | null,
-        public HSPF: number | null,
-        public SEER: number | null,
-        public Hcap17: number | null,
-        public Hcap47: number | null,
-        public fuelTypes: string[] | null,
-        public equipmentTags: number | null,
-        public AHRIReferences: number[] | null,
-        public availableRebates: number | null,
-        public furnaceInputBTUH: number | null,
-        public furnaceOutputBTUH: number | null,
-        public configurationOptions: object[] | null,
-        public coolingCapacityRated: number | null,
-        public furnaceConfigurations: object[] | null,
-        public totalAvailableRebates: number | null
-        
-    ) {}
-}
-
