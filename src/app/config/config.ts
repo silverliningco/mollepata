092ede1de50c47af75781ec02f2753d5c8232030
Atixcarlos <atix.figueroa@gmail.com>
2023-01-31 11:50:08 -0500

    credential

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,3 +1,3 @@
 export const URL_SERVICIOS = 'http://45.79.197.74';
-
-export const API_KEY = 'd4e04eb8-c535-4efd-a2db-4cc50e2562ef';
+export const API_KEY = '6f873e0e-1c16-4744-9d2f-0371377fd87a';
+//export const API_KEY = 'd4e04eb8-c535-4efd-a2db-4cc50e2562ef';
