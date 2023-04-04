2e98226f862b57995dbc9eefb6372d18ba564917
Atixcarlos <atix.figueroa@gmail.com>
2023-04-04 16:53:08 -0500

    updating changes

--- src/app/equipment-search/hvac-system-search/hvac-system-search.component.ts
+++ src/app/equipment-search/hvac-system-search/hvac-system-search.component.ts
@@ -169,6 +169,8 @@ export class HVACSystemSearchComponent implements OnInit {
       // To force change detection is to set a new object reference after modifying the property values
       this.payload = { ...this.myData };
     
+//this.payload = {"location":{"state":"MA","country":"US"},"utilityProviders":{"electricUtilityId":5,"fossilFuelUtilityId":4},"dwellingInfo":{"constructionType":"new-construction","fuelSource":"Natural Gas"},"systemDesign":[{"unitType":"Outdoor unit","systemType":"Split system","HVACType":"HP"}],"systemSize":{"heatingBTUH":58000,"coolingTons":2},"commerceInfo":{"eCommerceGatewayId":1,"stockStatus":"Stock"}}
+    
     }
 
   }
