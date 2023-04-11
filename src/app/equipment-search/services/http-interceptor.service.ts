a90c3ab92dcac1857adab43dd0340bfb32b60023
hmariaca <hildebrandomariaca@gmail.com>
2023-04-11 18:36:11 -0500

    add loader spinner

--- src/app/equipment-search/services/http-interceptor.service.ts
+++ src/app/equipment-search/services/http-interceptor.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { finalize } from 'rxjs/operators';
+//import { LoaderService } from './endpoints.service';
+import { LoaderService } from '../services/loader.service';
+
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -10,16 +14,28 @@ import { environment } from 'src/environments/environment';
 
 export class HttpInterceptorService implements HttpInterceptor{
 
-  constructor() {}
+  private totalRequests = 0;
+
+  constructor(private loadingService: LoaderService) {}  
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     request = request.clone({
       setHeaders: {
         'x-api-key': environment.apiKey
-      }
+      }      
     });
 
-    return next.handle(request);
-  }
+    this.totalRequests++;
+    this.loadingService.setLoading(true);
+
+    return next.handle(request).pipe(
+      finalize(() => {
+        this.totalRequests--;
+        if (this.totalRequests == 0) {
+          this.loadingService.setLoading(false);
+        }
+      })
+    );    
+  }  
 }
