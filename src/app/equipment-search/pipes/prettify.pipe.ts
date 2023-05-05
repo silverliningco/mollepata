d46ebfba00faa4ede756d7437bd557298c48e342
Atixcarlos <atix.figueroa@gmail.com>
2023-05-05 13:08:33 -0500

    prettify pipe

--- src/app/equipment-search/pipes/prettify.pipe.ts
+++ src/app/equipment-search/pipes/prettify.pipe.ts
@@ -4,12 +4,20 @@ import prettifyData from '../../../assets/json/prettify_data.json';
 @Pipe({
   name: 'prettify'
 })
+// This is a custom Angular pipe called "PrettifyPipe"
+// It takes a value and a rating string as input, and returns a transformed value
+// based on the specified rating.
 export class PrettifyPipe implements PipeTransform {
-  transform(value: string): string {     
-    const myPrettifyData = prettifyData[value as keyof typeof prettifyData]; 
-    if(myPrettifyData) {
-      return myPrettifyData;
+  // The transform function takes the input value and the rating string
+  // and returns the transformed value.
+  transform(value: any, rating: string): any {  
+    // Get the data type of the rating from the prettifyData JSON   
+    const myPrettifyData = prettifyData[rating as keyof typeof prettifyData].type; 
+    // If the data type is a number, return the value as a localized string
+    if(myPrettifyData =='number') {
+      return value.toLocaleString();
     } 
+    // Otherwise, return the value unchanged
     else {
       return value;
     }
