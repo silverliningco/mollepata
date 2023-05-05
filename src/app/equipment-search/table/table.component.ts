b39a9f25c8ec7325200e5867904f06ea6c4f009b
Atixcarlos <atix.figueroa@gmail.com>
2023-05-05 16:27:25 -0500

    Table update

--- src/app/equipment-search/table/table.component.ts
+++ src/app/equipment-search/table/table.component.ts
@@ -11,7 +11,7 @@ export class TableComponent implements OnInit {
 
   mySystems: any;
   JSON = JSON;
-  myColumnRatings = ["eer2", "seer2", "hspf2", "AFUE"];
+  myColumnRatings = ["EER2", "SEER2", "HSPF2", "AFUE"];
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
@@ -31,17 +31,44 @@ export class TableComponent implements OnInit {
     return components.filter((c:any) => c.componentType == componentType)[0];
   }
 
+  /**
+ * Gets the value of the rating that matches the given string
+ * @param ratings An array that contains the ratings in JSON format
+ * @param myString A string that represents the desired rating
+ * @returns The value of the rating as a string, or "N/A" if the rating is not found
+ */
   getRating(ratings: any[], myString: string) :string {
+    // Default value if rating is not found
     let ratingString = "N/A";
 
-    ratings.forEach( rating => { 
-      if(rating[myString]) {
-        ratingString = rating[myString];
-      }
-    });
-
+    // Switch statement to handle different types of ratings
+    switch (myString) {
+      case "EER2":
+      case "SEER2":
+      case "HSPF2":
+        // Find the rating in the 2023 array
+        const rating = ratings[0]['2023'].find((rating:any) => rating.rating === myString);
+        // If the rating is found
+        if(rating){
+          ratingString = rating.value;
+        }
+        break;
+  
+      case "AFUE":
+        // Check if there is a second array in the ratings array
+        if(ratings[1]){
+          // Find the rating in the second array
+          const rating2 = ratings[1].find((rating:any) => rating.rating === myString);
+          // If the rating is found
+          if(rating2){
+            ratingString = rating2.value;
+          }
+        }
+        break;
+    
+      default:
+        break;
+    }
     return ratingString;
-
   }
-
 }
