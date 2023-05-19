95de43006f2e91d6d972b9a00f3562062ee80482
Atixcarlos <atix.figueroa@gmail.com>
2023-05-19 11:23:37 -0500

    change api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiURL: 'https://devapi.coolcalc.com',
-  apiKey: 'ee60a2fd-db41-40c3-b237-47ecf743d59a',
-  //apiURL: 'http://45.79.197.74',
-  //apiKey: '6f873e0e-1c16-4744-9d2f-0371377fd87a',
+  //apiURL: 'https://devapi.coolcalc.com',
+  //apiKey: 'ee60a2fd-db41-40c3-b237-47ecf743d59a',
+  apiURL: 'http://45.79.197.74',
+  apiKey: '6f873e0e-1c16-4744-9d2f-0371377fd87a',
 };
