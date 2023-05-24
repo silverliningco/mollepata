675013f2a975efa8e2ffe791817d6c7eb74a7732
Atixcarlos <atix.figueroa@gmail.com>
2023-05-24 15:53:25 -0500

    Adding RebateUpTo to print

--- src/app/equipment-search/print/print.component.ts
+++ src/app/equipment-search/print/print.component.ts
@@ -12,6 +12,8 @@ export class PrintComponent implements OnInit {
   //pass Object to template, to iterate object keys using *ngFor (AHRI Ratings)
   Object = Object;
 
+  totalRebateUp = 0;
+
   constructor(
     public activatedRoute: ActivatedRoute) {
    }
@@ -19,6 +21,8 @@ export class PrintComponent implements OnInit {
   ngOnInit(): void {
     let res = this.activatedRoute.snapshot.paramMap.get('payload');
     this.result = JSON.parse(res!);
+    
+    this.totalRebateUp = this.sumRebateValues(this.result!.rebateEligibility)
   }
 
   getComponentByComponentType(components: any[], componentType:string) {
@@ -27,6 +31,17 @@ export class PrintComponent implements OnInit {
       return myComponentByType[0];
     }
     return 0;
+  }
+
+  /**
+ * Calculates the sum of rebate values from an array of objects.
+ * @param rebateEligibility An array of objects containing rebate information.
+ * @returns The total sum of rebate values.
+ */
+  sumRebateValues(rebateEligibility: any[]): number {
+    return rebateEligibility.reduce((accumulator, object) => {
+      return accumulator + object["rebateUpTo"];
+    }, 0);
   }
 
   // open window dialog to print.
